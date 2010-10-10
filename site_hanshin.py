996852f99a483b8ec897412ab467ab4394649f3b
Frank Lai <Frank Lai@localhost>
2010-10-10 17:35:58 +0800

    - fix Hanshin

--- site_hanshin.py
+++ site_hanshin.py
@@ -1,63 +1,70 @@
+import logging
 import os
 import re
-import struct
 from threading import Thread
 import urllib
-import urllib2
 import urlparse
-import zlib
+
+import common
 
 site_index = 'hanshin'
 site_keyword = 'hanshin'
 site_url = 'http://www.hanshinarena.com.tw/'
-test_url = 'http://www.hanshinarena.com.tw/03-2/n_list01.php?m_id=137'
+test_url = 'http://www.hanshinarena.com.tw/DM/?m_id=39'
 
-def get_first_match(pattern, string):
-    result = ''
-    
-    regex = re.compile(pattern).search(string)
-    if regex:
-        result = regex.group(1)
+def get_id(url):
+    pattern = u'm_id=([0-9]+)'
+    id = common.get_first_match(pattern, url)
 
-    return result
+    if id == '':
+        pattern = u'/DM/([0-9]+)'
+        id = common.get_first_match(pattern, url)
 
-def get_content_by_url(url):
-    f = urllib2.urlopen(url)
-    html = f.read()
+    return id
 
-    return html
+def get_title(url, id):
+    # http://www.hanshinarena.com.tw/DM/bottom.php
+    # http://www.hanshin.com.tw/DM/bottom.php
+    # get title from list
+    listUrl = urlparse.urljoin(url, '/DM/bottom.php')
+    logging.debug('bottom url: %s' % (listUrl))
 
-def get_title(url):
-    pos = url.find('=')
-    title = url[pos + 1:]
+    html = common.get_content_by_url(listUrl)
+    html = html.decode('utf-8')
 
-    html = get_content_by_url(url)
-    html = html.decode('utf8')
+    pattern = u'<title>([^<]+)</title>'
+    department = common.get_first_match(pattern, html)
+    logging.debug('department: %s' % (department.encode('big5')))
 
-    pattern = '<div align="center"><strong>([^<]+)</strong></div>'
-    title += get_first_match(pattern, html)
-    print title
+    pattern = u'option value="([0-9]+)" *>([^<]+)<'
+    titles = re.compile(pattern).findall(html)
+    logging.debug('titles: %s' % (repr(titles)))
+
+    matchedTitle = ''
+
+    for titleId, titleStr in titles:
+        if titleId == id:
+            matchedTitle = titleStr
+            break
+            
+    title = '%s - %s' % (department, matchedTitle)
+    logging.debug('title: %s' % (title.encode('big5')))
 
     return title
 
-def get_swfs(url):
-    html = get_content_by_url(url)
-    html = html.decode('utf8')
+def get_jpgs(url, id):
+    # get values from xml
+    # http://www.hanshinarena.com.tw/DM/39/Pages.xml
+    xmlUrl = urlparse.urljoin(url, '/DM/%s/Pages.xml' % (id))
+    xml = common.get_content_by_url(xmlUrl)
 
-    pattern = "ursor:hand onClick=\"MM_openBrWindow\('([^']+)'"
-    swfs = re.compile(pattern).findall(html)
+    pattern = u'<page src="([^"]+)"/>'
+    imgs = re.compile(pattern).findall(xml)
 
-    swfs = [urlparse.urljoin(url, swf) for swf in swfs]
+    # http://www.hanshinarena.com.tw/DM/39/pages/4.jpg
+    full_imgs = [urlparse.urljoin(xmlUrl, img) for img in imgs]
 
-    return swfs
-
-def get_jpgs(url):
-    html = get_content_by_url(url)
-
-    pattern = 'img src="([^"]+)"'
-    imgs = re.compile(pattern).findall(html)
-
-    full_imgs = [urlparse.urljoin(url, img.replace('S.jpg', 'B.jpg')) for img in imgs]
+    logging.debug('images: %s' % (repr(full_imgs)))
 
     return full_imgs
 
@@ -71,26 +78,23 @@ class DownloadFile(Thread):
     def run(self):
         urllib.urlretrieve(self.url, self.path)
 
-def download_swfs(title, swfs):
+def download_jpgs(title, jpgs):
     cwd = os.getcwdu()
 
-    path_prefix = os.path.join(cwd, title, 'SWFs')
+    path_prefix = os.path.join(cwd, title)
 
     if not os.path.exists(path_prefix):
         # mkdir if directory not exists
         os.makedirs(path_prefix)
 
-    length = len(swfs)
+    length = len(jpgs)
     index = 0
     print('Start to downloading %s (total: %d)' % (title.encode('big5'), length))
 
-    local_files = []
-
-    for swf in swfs:
+    for jpg in jpgs:
         index += 1
 
-        pos = swf.rfind('/')
-        filename = swf[pos + 1:]
+        filename = jpg[jpg.rfind('/') + 1:]
 
         path = os.path.join(path_prefix, filename)
 
@@ -98,74 +102,30 @@ def download_swfs(title, swfs):
             print('(%d/%d) skip %s, already exists' % (index, length, path,))
             continue
 
-        task = DownloadFile(swf, path)
+        task = DownloadFile(jpg, path)
         task.start()
 
         # wait until the thread finish
         task.join()
 
-        local_files.append(path)
-
-        print('finish(%d/%d): %s' % (index, length, path,))
-        
-    return local_files
-
-def extract_jpg_from_swf(local_files):
-    if len(local_files) == 0:
-        return
-
-    path_prefix = os.path.dirname(local_files[0]).replace('SWFs', 'JPGs')
-
-    if not os.path.exists(path_prefix):
-        # mkdir if directory not exists
-        os.makedirs(path_prefix)
-
-    for swf in local_files:
-        bytes = open(swf, 'rb').read()
-        
-        if bytes[0:3] == 'CWS':
-            compressed_str = bytes[8:]
-            uncompressed_str = zlib.decompress(compressed_str)
-
-            mark = struct.pack('BBBB', 0xFF, 0xD8, 0xFF, 0xE0)
-            pos = uncompressed_str.find(mark)
-
-            length_bytes = uncompressed_str[pos - 6: pos - 2]
-            length = struct.unpack('<I', length_bytes)[0]
-            length -= 2
-
-            jpg_path = os.path.join(path_prefix, os.path.split(swf)[1])
-            jpg_path = jpg_path[:jpg_path.rfind('.')] + '.jpg'
-
-            open(jpg_path, 'wb').write(uncompressed_str[pos:pos+length])
-
-#         open('un.swf', 'wb').write(uncompressed_str)
+        print('finish(%d/%d): %s' % (index, length, jpg,))
 
 def downloader(url):
-    title = get_title(url)
+    id = get_id(url)
+    title = get_title(url, id)
 
-    swfs = get_swfs(url)
+    jpgs = get_jpgs(url, id)
 
-    local_files = download_swfs(title, swfs)
+    download_jpgs(title, jpgs)
 
-    extract_jpg_from_swf(local_files)
-
-def main(input):
-    for line in open(input, 'rb'):
-        line = line.strip()
-
-        if len(line) == 0:
-            # skip blank line
-            continue
-
-        if line[0] == '#':
-            # skip comment
-            continue
-
-        downloader(line)
-
+def main():
+    downloader(test_url)
 
 if __name__ == '__main__':
-    input = 'input_hanshin.txt'
-    main(input)
+    logging.basicConfig(level=logging.DEBUG)
+    main()
+    # http://www.hanshinarena.com.tw/DM/39/
+    # http://www.hanshinarena.com.tw/DM/?m_id=39
+    # http://www.hanshinarena.com.tw/DM/39/Pages.xml
+    # http://www.hanshinarena.com.tw/DM/39/pages/4.jpg
 
