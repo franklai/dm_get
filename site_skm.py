bb9926fb772866636e7f16c278f53ad8e0e3d0d3
Frank Lai <Frank Lai@localhost>
2010-10-10 18:09:21 +0800

    - modify debug code

--- site_skm.py
+++ site_skm.py
@@ -110,25 +110,8 @@ def downloader(url):
     download_jpgs(title, jpgs)
 
 def main():
-    input = 'input_skm.txt'
-
-    for line in open(input, 'rb'):
-        line = line.strip()
-
-        if len(line) == 0:
-            # skip blank line
-            continue
-
-        if line[0] == '#':
-            # skip comment
-            continue
-
-        print line
-        downloader(line)
-
+    downloader(test_url)
 
 if __name__ == '__main__':
-#     r = urllib2.urlopen('http://www.sogo.com.tw/NEWSOGO/04/flippingbook/091006_1018AWjp/index.html')
-#     html = r.read()
-#     print html
+    logging.basicConfig(level=logging.DEBUG)
     main()
