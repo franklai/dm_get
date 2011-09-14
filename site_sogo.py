993bbfeced18e47bbe5e379cce3ed20b48841179
Frank Lai <Frank Lai@localhost>
2011-09-14 22:08:39 +0800

    fix sogo

--- site_sogo.py
+++ site_sogo.py
@@ -8,7 +8,19 @@ import common
 site_index = 'sogo'
 site_keyword = 'sogo'
 site_url = 'http://www.sogo.com.tw/'
-test_url = 'http://www.sogo.com.tw/www/flippingbook/index.asp?id=199&backflag=1'
+test_url = 'http://www.sogo.com.tw/www/flippingbook/index.asp?id=427&BranchIds=1&backflag=1'
+# test_url = 'http://www.sogo.com.tw/www/flippingbook/index2.asp?id=432&sp=&flag=&BranchIds=2'
+
+
+def get_real_content_by_html(url, html):
+    pattern = '<frame src="(index[^"]+)"'
+    frame_url = common.get_first_match(pattern, html)
+    logging.debug('real content frame url is [%s]' % (frame_url, ))
+
+    realUrl = urlparse.urljoin(url, frame_url)
+    realHtml = common.get_content_by_url(realUrl)
+
+    return realHtml
 
 def get_charset(html):
     pattern = 'charset=([^"]+)'
@@ -21,7 +33,7 @@ def get_title(url, html):
     title = common.get_first_match(pattern, html)
     logging.debug('title: %s' % (title.encode('big5')))
 
-    return title
+    return title.strip()
 
 def get_pages(html):
     prefix = 'flippingBook.pages'
@@ -59,11 +71,17 @@ def get_jpgs(url, html):
 
 def downloader(url):
     html = common.get_content_by_url(url)
+
+    if -1 == html.find('swfobject'):
+        # Cannot find swfobject, may be iframe page
+        html = get_real_content_by_html(url, html)
+
     charset = get_charset(html)
     html = html.decode(charset)
     title = get_title(url, html)
     jpgs = get_jpgs(url, html)
 
+    logging.debug('title: [%s], ' % (title, ))
     common.download_jpgs(title, jpgs)
 
 def main():
