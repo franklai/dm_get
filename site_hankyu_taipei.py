476e6ebf828d93f76798e45ef9339d3204d6a4a0
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2012-08-31 22:39:16 +0800

    fix hankyu taipei

--- site_hankyu_taipei.py
+++ site_hankyu_taipei.py
@@ -8,123 +8,72 @@ import common
 site_index = 'hankyu_taipei'
 site_keyword = 'uni-hankyu.com.tw/taipei'
 site_url = 'http://www.uni-hankyu.com.tw/taipei/'
-test_url = 'http://www.uni-hankyu.com.tw/taipei/upload/dm/dm_no1.html'
+test_url = 'http://www.uni-hankyu.com.tw/taipei/upload/dm/no48/index.html'
 
-def get_swf_link(url, html):
-    pattern = u'SwfName=([a-z0-9]+/book.swf)'
-    swfFile = common.get_first_match(pattern, html)
+def get_charset(html):
+    pattern = 'charset=([^"]+)'
+    charset = common.get_first_match(pattern, html)
+    logging.debug('charset = %s' % (charset, ))
+    return charset
 
-    pos = url.rfind('/')
-    swfLink = url[:pos] + '/' + swfFile
+def get_title_from_html(html):
+    # use directory as part of title
+    pattern = u'<title>([^<]*)</title>'
+    prefix = common.get_first_match(pattern, html)
 
-    logging.debug('swf file name: %s' % (swfFile))
-    logging.debug('swf link: %s' % (swfLink))
+    pattern = "var title = '([^']+)';"
+    dmTitle = common.get_first_match(pattern, html)
 
-    return swfLink
+    title = '%s - %s' % (prefix, dmTitle)
 
-def get_title(url):
-    dmMainUrl = 'http://www.uni-hankyu.com.tw/taipei/dm.asp'
-    html = common.get_content_by_url(dmMainUrl)
-    html = html.decode('utf-8')
-
-    pattern = u'<title>([^<]+)</title>'
-    department = common.get_first_match(pattern, html)
-
-    pos = url.rfind('/')
-    urlFile = url[pos + 1]
-
-    urlPos = html.find(urlFile)
-    titlePos = html.find('<strong>', urlPos)
-    if titlePos > 0:
-        endPos = html.find('</strong>', titlePos)
-        matchedTitle = html[titlePos + len('<strong>'): endPos]
-
-        title = '%s - %s' % (department, matchedTitle)
-    else:
-        title = '%s - %s' % (department, urlFile)
-
-    logging.debug('title: %s' % (title.encode('big5')))
+    logging.debug('title = %s' % (title, ))
 
     return title
 
-def get_swf(swfLink):
-    print('Start download dm swf, may take a little time.')
-    bytes = common.get_content_by_url(swfLink)
+def get_book_settings_content(url, html):
+    pattern = '"([^"]*bookSettings.*.js)"'
+    js_url = common.get_first_match(pattern, html)
 
-    return bytes
+    js_full_url = urlparse.urljoin(url, js_url)
+    
+    logging.debug('js_full_url:' + js_full_url)
 
-def get_jpgs(swfLink):
-    index = 30
-    step = stepMax = 16
-    forward = True
+    js = common.get_content_by_url(js_full_url)
+#     logging.debug('js:' + js)
 
-    while abs(step) > 0:
-        jpgUrl = urlparse.urljoin(swfLink, '%02d.jpg' % (index))
-        logging.debug('test url is %s' % (jpgUrl, ))
+    return js
 
-        if common.is_url_ok(jpgUrl):
-            if step != stepMax:
-                step /= 2
-            forward = True
-        else:
-            step /= 2
-            forward = False
+def get_pages_from_js(js):
+    pattern = '"pages/([^"]+)'
+    jpgs = common.get_all_matched(pattern, js)
 
-        if forward:
-            index += step
-        else:
-            index -= step
-
-    lastIndex = index
-    if not forward:
-        lastIndex -= 1
-
-    logging.debug('last index is %d' % (lastIndex))
-
-    jpgs = [urlparse.urljoin(swfLink, '%02d.jpg' % (x, )) for x in range(lastIndex + 1)] 
     return jpgs
+    
+def get_jpgs(url, html):
+    pos = url.rfind('/')
+    prefix = url[:pos]
 
-def download_jpgs(title, jpgs):
-    cwd = os.getcwdu()
+    js = get_book_settings_content(url, html)
 
-    path_prefix = os.path.join(cwd, title)
+    jpgs = get_pages_from_js(js)
 
-    if not os.path.exists(path_prefix):
-        # mkdir if directory not exists
-        os.makedirs(path_prefix)
+    logging.debug('prefix:' + prefix)
+    logging.debug('jpgs:' + repr(jpgs))
 
-    length = len(jpgs)
-    index = 0
-    print('Start to downloading %s (total: %d)' % (title.encode('big5'), length))
+    full_jpgs = ['%s/pages/large/%s' % (prefix, jpg) for jpg in jpgs]
 
-    for jpg in jpgs:
-        index += 1
-
-        filename = jpg[jpg.rfind('/') + 1:]
-
-        path = os.path.join(path_prefix, filename)
-
-        if os.path.exists(path) and os.path.getsize(path) > 0:
-            print('(%d/%d) skip %s, already exists' % (index, length, path,))
-            continue
-
-        task = common.DownloadFile(jpg, path)
-        task.start()
-
-        # wait until the thread finish
-        task.join()
-
-        print('finish(%d/%d): %s' % (index, length, jpg,))
+    return full_jpgs
 
 def downloader(url):
     html = common.get_content_by_url(url)
-    html = html.decode('big5')
-    swfLink = get_swf_link(url, html)
-    title = get_title(url)
+    charset = get_charset(html)
+    html = html.decode(charset)
 
-    jpgs = get_jpgs(swfLink)
+    title = get_title_from_html(html)
 
-    download_jpgs(title, jpgs)
+    jpgs = get_jpgs(url, html)
+
+    common.download_jpgs(title, jpgs)
 
 def main():
     downloader(test_url)
