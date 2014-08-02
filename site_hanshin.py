6b27ec3e4729ec54523fa7c46d8ae4774a505c05
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2014-08-02 21:44:36 +0800

    refactoring hanshin

--- site_hanshin.py
+++ site_hanshin.py
@@ -1,8 +1,4 @@
 import logging
-import os
-import re
-from threading import Thread
-import urllib
 import urlparse
 
 import common
@@ -11,7 +7,7 @@ site_index = 'hanshin'
 site_keyword = 'hanshin'
 site_url = 'http://www.hanshinarena.com.tw/'
 # test_url = 'http://www.hanshinarena.com.tw/DM/?m_id=69'
-test_url = 'http://www.hanshin.com.tw/DM/?m_id=70'
+test_url = 'http://www.hanshin.com.tw/DM/?m_id=164'
 
 def get_id(url):
     pattern = u'm_id=([0-9]+)'
@@ -38,7 +34,7 @@ def get_title(url, id):
     logging.debug('department: %s' % (department.encode('big5')))
 
     pattern = u'option value="([0-9]+)" *>([^<]+)<'
-    titles = re.compile(pattern).findall(html)
+    titles = common.get_all_matched(pattern, html)
     logging.debug('titles: %s' % (repr(titles)))
 
     matchedTitle = ''
@@ -47,8 +43,11 @@ def get_title(url, id):
         if titleId == id:
             matchedTitle = titleStr
             break
-            
-    title = '%s - %s' % (department, matchedTitle)
+
+    if matchedTitle:
+        title = '%s - %s' % (department, matchedTitle)
+    else:
+        title = '%s %s' % (department, id)
     logging.debug('title: %s' % (title.encode('big5')))
 
     return title
@@ -57,10 +56,11 @@ def get_jpgs(url, id):
     # get values from xml
     # http://www.hanshinarena.com.tw/DM/39/Pages.xml
     xmlUrl = urlparse.urljoin(url, '/DM/%s/Pages.xml' % (id))
+    logging.debug(xmlUrl)
     xml = common.get_content_by_url(xmlUrl)
 
     pattern = u'<page src="([^"]+)"/>'
-    imgs = re.compile(pattern).findall(xml)
+    imgs = common.get_all_matched(pattern, xml)
 
     # http://www.hanshinarena.com.tw/DM/39/pages/4.jpg
     full_imgs = [urlparse.urljoin(xmlUrl, img) for img in imgs]
@@ -69,58 +69,18 @@ def get_jpgs(url, id):
 
     return full_imgs
 
-
-class DownloadFile(Thread):
-    def __init__(self, url, path):
-        Thread.__init__(self)
-        self.url = url
-        self.path = path
-
-    def run(self):
-        urllib.urlretrieve(self.url, self.path)
-
-def download_jpgs(title, jpgs):
-    cwd = os.getcwdu()
-
-    path_prefix = os.path.join(cwd, title)
-
-    if not os.path.exists(path_prefix):
-        # mkdir if directory not exists
-        os.makedirs(path_prefix)
-
-    length = len(jpgs)
-    index = 0
-    print('Start to downloading %s (total: %d)' % (title.encode('big5'), length))
-
-    for jpg in jpgs:
-        index += 1
-
-        filename = jpg[jpg.rfind('/') + 1:]
-
-        path = os.path.join(path_prefix, filename)
-
-        if os.path.exists(path) and os.path.getsize(path) > 0:
-            print('(%d/%d) skip %s, already exists' % (index, length, path,))
-            continue
-
-        task = DownloadFile(jpg, path)
-        task.start()
-
-        # wait until the thread finish
-        task.join()
-
-        print('finish(%d/%d): %s' % (index, length, jpg,))
-
 def downloader(url):
     id = get_id(url)
     title = get_title(url, id)
 
     jpgs = get_jpgs(url, id)
 
-    download_jpgs(title, jpgs)
+    common.download_jpgs(title, jpgs)
 
 def main():
-    downloader(test_url)
+    url = test_url
+#     url = 'http://www.hanshinarena.com.tw/DM/?m_id=147'
+    downloader(url)
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.DEBUG)
