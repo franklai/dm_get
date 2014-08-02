55f49d3351d764ab7d2cf2e590386e7dcb723083
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2014-08-02 21:23:07 +0800

    fix feds

--- site_feds.py
+++ site_feds.py
@@ -1,45 +1,25 @@
 import logging
-import os
-import re
-from threading import Thread
-import urllib
-import urllib2
 import urlparse
+import common
 
 site_index = 'feds'
 site_keyword = 'feds'
 site_url = 'http://www.feds.com.tw/'
-test_url = 'http://www.feds.com.tw/ebook/index.asp?ParentSNo=592'
-
-
-def get_first_match(pattern, string):
-    result = ''
-    
-    regex = re.compile(pattern).search(string)
-    if regex:
-        result = regex.group(1)
-
-    return result
-
-def get_content_by_url(url):
-    f = urllib2.urlopen(url)
-    html = f.read()
-
-    return html
+test_url = 'http://www.feds.com.tw/store/dm.aspx?store=16&dm=20140731153420f55'
 
 def get_title(url, html):
     # use directory as part of title
     title = u'feds '
 
-    pattern = u'\n - ([^<]*)</title>'
-    title += get_first_match(pattern, html)
+    pattern = u'<title>([^<]*)</title>'
+    title += common.get_first_match(pattern, html)
     logging.debug(title)
 
     return title.strip()
 
 def get_jpgs(url, html):
-    pattern = '"(\.\./upload/edm/.*/small/[^"]+)"'
-    jpgs = re.compile(pattern).findall(html)
+    pattern = '"(\.\./photo/dm/small/[^"]+)"'
+    jpgs = common.get_all_matched(pattern, html)
     logging.debug('jpgs:' + repr(jpgs))
 
     full_jpgs = [urlparse.urljoin(url, jpg.replace('small', 'large')) for jpg in jpgs] 
@@ -47,55 +27,18 @@ def get_jpgs(url, html):
 
     return full_jpgs
 
-class DownloadJpg(Thread):
-    def __init__(self, url, path):
-        Thread.__init__(self)
-        self.url = url
-        self.path = path
-
-    def run(self):
-        urllib.urlretrieve(self.url, self.path)
-
-def download_jpgs(title, jpgs):
-    cwd = os.getcwdu()
-
-    path_prefix = os.path.join(cwd, title)
-
-    if not os.path.exists(path_prefix):
-        # mkdir if directory not exists
-        os.makedirs(path_prefix)
-
-    length = len(jpgs)
-    index = 0
-    print('Start to downloading %s (total: %d)' % (title.encode('big5'), length))
-
-    for jpg in jpgs:
-        index += 1
-
-        filename = jpg[jpg.rfind('/') + 1:]
-
-        path = os.path.join(path_prefix, filename)
-
-        if os.path.exists(path) and os.path.getsize(path) > 0:
-            print('(%d/%d) skip %s, already exists' % (index, length, path,))
-            continue
-
-        task = DownloadJpg(jpg, path)
-        task.start()
-
-        # wait until the thread finish
-        task.join()
-
-        print('finish(%d/%d): %s' % (index, length, jpg,))
-
 
 def downloader(url):
-    html = get_content_by_url(url)
-    html = html.decode('big5')
+    # replace url to flash book
+    url = url.replace('/store/dm.aspx', '/store/book.aspx')
+
+    logging.debug('url: %s' % (url,))
+    html = common.get_content_by_url(url)
+    html = html.decode('utf-8')
     title = get_title(url, html)
     jpgs = get_jpgs(url, html)
 
-    download_jpgs(title, jpgs)
+    common.download_jpgs(title, jpgs)
 
 def main():
     downloader(test_url)
