b01bc17410102a687be14d7b7d5ff33c3621c06f
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2014-08-02 17:35:02 +0800

    update skm dm parse

--- site_skm.py
+++ site_skm.py
@@ -8,15 +8,40 @@ site_index = 'skm'
 site_keyword = 'skm'
 site_url = 'http://www.skm.com.tw/'
 # test_url = 'http://www.skm.com.tw/edm/str120/2012_0830_autumn/index.html'
-test_url = 'http://www.skm.com.tw/edm/?st=str350&dm=20120824_AUTUMN'
+# test_url = 'http://www.skm.com.tw/edm/?st=str350&dm=20120824_AUTUMN'
+test_url = 'http://www.skm.com.tw/zh-TW/OnLineDM/Detail?CTID=25b8fd11-9504-443a-ab5f-02c88a5998ec&storeId=b5d3a2fc-b9fa-4486-a9ba-4acae9ed5f9c'
+
+def parse_query(url):
+    o = urlparse.urlparse(url)
+    queries = urlparse.parse_qs(o.query)
+
+    return queries
+
+def get_dm_id(url):
+    q = parse_query(url)
+
+    return q['CTID'][0]
+
+def get_store_id(url):
+    q = parse_query(url)
+
+    return q['storeId'][0]
+
+def get_title_by_value(value, html):
+    pattern = 'value="%s"[a-z ]*>(.*?)</option>' % (value)
+    logging.debug('pattern: %s' % (pattern))
+    return common.get_first_match(pattern, html)
 
 def get_title(url, html):
-    # use directory as part of title
-    pattern = '/([^/]+)/[^/]+$'
-    title = common.get_first_match(pattern, url)
+    store_id = get_store_id(url)
+    dm_id = get_dm_id(url)
 
-    pattern = u'<title>([^<]*)</title>'
-    title += common.get_first_match(pattern, html)
+    logging.debug('id, store: %s, dm: %s' % (store_id, dm_id))
+
+    store_title = get_title_by_value(store_id, html)
+    dm_title = get_title_by_value(dm_id, html)
+
+    title = '%s - %s' % (store_title, dm_title)
 
     logging.debug('title = %s' % (title, ))
 
@@ -28,95 +53,28 @@ def get_charset(html):
     logging.debug('charset = %s' % (charset, ))
     return charset
 
-def get_book_settings_content(url, html):
-    pattern = '"([^"]*bookSettings.*.js)"'
-    js_url = common.get_first_match(pattern, html)
-
-    js_full_url = urlparse.urljoin(url, js_url)
-    
-    logging.debug('js_full_url:' + js_full_url)
-
-    js = common.get_content_by_url(js_full_url)
-#     logging.debug('js:' + js)
-
-    return js
-
-def get_pages_from_js(js):
-    pattern = '"pages/([^"]+)'
-    jpgs = common.get_all_matched(pattern, js)
-
-    return jpgs
+def find_thumbs(dm_id, html):
+    pattern = '<img src="(/upload/%s/.*?)" />' % (dm_id)
+    thumbs = common.get_all_matched(pattern, html)
+    return thumbs
 
 def get_jpgs(url, html):
-    pos = url.rfind('/')
-    prefix = url[:pos]
+    dm_id = get_dm_id(url)
+    thumbs = find_thumbs(dm_id, html)
 
-    js = get_book_settings_content(url, html)
+    jpgs = [urlparse.urljoin(url, thumb.replace('/z_', '/')) for thumb in thumbs]
 
-    jpgs = get_pages_from_js(js)
-
-    logging.debug('prefix:' + prefix)
     logging.debug('jpgs:' + repr(jpgs))
 
-    full_jpgs = ['%s/pages/large/%s' % (prefix, jpg) for jpg in jpgs]
-
-    return full_jpgs
-
-def get_title_from_js(js):
-    pattern = 'var eDmTitle="([^"]+)";'
-    title = common.get_first_match(pattern, js)
-
-    return title
-
-def get_prefix_from_url(url):
-    # http://www.skm.com.tw/edm/?st=str350&dm=20120824_summersale
-    # http://www.skm.com.tw/edm/str350/20120824_summersale/pages/large/01.jpg
-
-    o = urlparse.urlparse(url)
-    queries = urlparse.parse_qs(o.query)
-    prefix = '%s://%s%s%s/%s' % (
-        o.scheme, o.netloc, o.path, 
-        queries['st'][0], queries['dm'][0]
-    )
-
-    return prefix
-
-def handle_new_style(url, html):
-    title = 'skm edm'
-    jpgs = []
-
-    pos = url.rfind('/')
-    prefix = url[:pos]
-
-    js = get_book_settings_content(url, html)
-    js = js.decode('utf-8')
-
-    jpgs = get_pages_from_js(js)
-    prefix = get_prefix_from_url(url)
-    full_jpgs = ['%s/pages/large/%s' % (prefix, jpg) for jpg in jpgs]
-    logging.debug('prefix:' + prefix)
-    logging.debug('jpgs:' + repr(jpgs))
-
-    eDmTitle = get_title_from_js(js)
-    pattern = 'document.title="([^"]+)"'
-    title = common.get_first_match(pattern, html) + eDmTitle
-
-    logging.debug('title:' + title)
-
-    return title, full_jpgs
+    return jpgs
 
 def downloader(url):
     html = common.get_content_by_url(url)
     charset = get_charset(html)
     html = html.decode(charset)
 
-    if charset == 'big5':
-        # old style
-        title = get_title(url, html)
-        jpgs = get_jpgs(url, html)
-    else:
-        # new style, only ZuoYing uses this (2012 Aug 31)
-        title, jpgs = handle_new_style(url, html)
+    title = get_title(url, html)
+    jpgs = get_jpgs(url, html)
 
     common.download_jpgs(title, jpgs)
 
@@ -124,6 +82,8 @@ def main():
     url = test_url
 #     url = 'http://www.skm.com.tw/edm/str300/20120820_hakka/index.html'
 #     url = 'http://www.skm.com.tw/edm/10/201208_midAutumn/index.html'
+#     url = 'http://www.skm.com.tw/zh-TW/OnLineDM/Detail?CTID=5c930a1c-0f38-414a-b987-76fd6199ce1d&storeId=9e7f2731-ff3c-4d18-b872-ebb2660528f5'
+#     url = 'http://www.skm.com.tw/zh-TW/OnLineDM/Detail?CTID=e647ea24-b4c3-48e3-b3c8-259217cfad5c&storeId=9daa4b7d-1a00-4cb9-a429-8cb99050e88a#1'
     downloader(url)
 
 if __name__ == '__main__':
