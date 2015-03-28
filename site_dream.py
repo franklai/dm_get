07bf7a62ffbe233355295f220da123e64ca1ba39
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2015-03-28 22:21:22 +0800

    fix dreammall and hankyu kaohsiung

--- site_dream.py
+++ site_dream.py
@@ -1,108 +1,101 @@
+# coding: utf-8
 import logging
-import os
-import re
-from threading import Thread
-import urllib
-import urllib2
 import urlparse
+
+import common
+import requests
 
 site_index = 'dream'
 site_keyword = 'dream-mall'
 site_url = 'http://www.dream-mall.com.tw/'
-test_url = 'http://www.dream-mall.com.tw/ecatalog/newindex.asp'
+test_url = 'http://www.dream-mall.com.tw/EDM'
 
-def get_first_match(pattern, string):
-    result = ''
-    
-    regex = re.compile(pattern).search(string)
-    if regex:
-        result = regex.group(1)
-
-    return result
-
-def get_content_by_url(url):
-    f = urllib2.urlopen(url)
-    html = f.read()
-
-    return html
-
-def get_title(url, html):
-    # use directory as part of title
-    pattern = u'<title>([^<]+)</title>'
-    title = get_first_match(pattern, html) + ' - '
-    logging.debug(title.encode('big5'))
-
-    pattern = '/EDM/([0-9]+)/'
-    title += get_first_match(pattern, html)
-    logging.debug(title.encode('big5'))
-
-    return title.strip()
-
-def get_jpgs(url, html):
-    pattern = u'class="option" href="([^"]+)" target="_blank"'
-    jpgs = re.compile(pattern).findall(html)
-    logging.debug('jpgs:' + repr(jpgs))
-
-    o = urlparse.urlparse(url)
-    urlPrefix = 'http://%s' % (o.netloc)
-    
-    full_jpgs = [urlparse.urljoin(urlPrefix, jpg) for jpg in jpgs]
-    logging.debug('full_jpgs:' + repr(full_jpgs))
-
-    return full_jpgs
-
-class DownloadJpg(Thread):
-    def __init__(self, url, path):
-        Thread.__init__(self)
+class Parser:
+    def __init__(self, url):
         self.url = url
-        self.path = path
+        self.s = requests.Session()
 
-    def run(self):
-        urllib.urlretrieve(self.url, self.path)
+        self.site = 'www.dream-mall.com.tw'
+        self.is_dream_mall = True
 
-def download_jpgs(title, jpgs):
-    cwd = os.getcwdu()
+        url = 'http://%s/' % (self.site, )
+        self.s.get(url)
 
-    path_prefix = os.path.join(cwd, title)
+    def switch_to_hankyu(self):
+        url = 'http://%s/Home/ChangeIsDM?isDreamMall=false' % (self.site, )
+        self.s.get(url)
+        self.is_dream_mall = False
 
-    if not os.path.exists(path_prefix):
-        # mkdir if directory not exists
-        os.makedirs(path_prefix)
+    def get_guids(self):
+        url = 'http://%s/EDM/' % (self.site, )
+        r = self.s.get(url)
+        html = r.text
 
-    length = len(jpgs)
-    index = 0
-    print('Start to downloading %s (total: %d)' % (title.encode('big5'), length))
+        pattern = 'data-guid="(.*?)"'
+        guids = common.get_all_matched(pattern, html)
 
-    for jpg in jpgs:
-        index += 1
+        logging.debug('guids: %s' % (repr(guids), ))
 
-        filename = jpg[jpg.rfind('/') + 1:]
+        return guids
 
-        path = os.path.join(path_prefix, filename)
+    def get_page_by_guid(self, guid):
+        url = 'http://%s/Edm/Detail' % (self.site, )
 
-        if os.path.exists(path) and os.path.getsize(path) > 0:
-            print('(%d/%d) skip %s, already exists' % (index, length, path,))
-            continue
+        r = self.s.post(url, {'guid': guid})
 
-        task = DownloadJpg(jpg, path)
-        task.start()
+        return r.text
 
-        # wait until the thread finish
-        task.join()
+    def get_title(self, html):
+        pattern = '<h3>(.*?)</h3>'
 
-        print('finish(%d/%d): %s' % (index, length, jpg,))
+        dm_title = common.get_first_match(pattern, html)
+
+        if self.is_dream_mall:
+            store = u'夢時代'
+        else:
+            store = u'統一阪急高雄店'
+
+        title = u'%s - %s' % (store, dm_title)
+        title = title.strip()
+
+        return title
+    
+    def get_jpgs(self, html):
+        pattern = '(/Upload/EDM/[^"]+/large_[0-9]+.jpg)'
+
+        jpgs = common.get_all_matched(pattern, html)
+
+        url_prefix = 'http://%s' % (self.site)
+        full_jpgs = [urlparse.urljoin(url_prefix, jpg) for jpg in jpgs]
+        logging.debug('full_jpgs:' + repr(full_jpgs))
+
+        return full_jpgs
 
 def downloader(url):
-    html = get_content_by_url(url)
-    html = html.decode('utf8', 'ignore')
-    title = get_title(url, html)
-    jpgs = get_jpgs(url, html)
+    # dreammall use ajax based to show dm, so cannot find out dm from url
+    # so we just download all dm
 
-    download_jpgs(title, jpgs)
+    # this request is to get cookie
+    parser = Parser(url)
+
+    def run_download():
+        guids = parser.get_guids()
+
+        for guid in guids:
+            html = parser.get_page_by_guid(guid)
+            title = parser.get_title(html)
+            jpgs = parser.get_jpgs(html)
+
+            common.download_jpgs(title, jpgs)
+
+    run_download() # for dreammall
+
+    parser.switch_to_hankyu()
+    run_download() # for hankyu kaohsiung
 
 def main():
     url = test_url
-    url = 'http://www.dream-mall.com.tw/ecatalog/newindex.asp?num=1&pic=%2FEcatalog%2FEDM%2F140730%2Fzoom_page01.jpg&pic2=%2FEcatalog%2FEDM%2F140730%2Fnormal_page01.jpg'
+    url = ''
     downloader(url)
 
 
