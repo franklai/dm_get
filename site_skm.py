e17434d4490fcc0098faf8f6296e3cbfbc7e2278
Frank Lai <franklai@cmlab.csie.ntu.edu.tw>
2016-08-29 01:09:18 +0800

    fix skm

--- site_skm.py
+++ site_skm.py
@@ -1,4 +1,5 @@
 # -*- coding: utf8 -*-
+import json
 import logging
 import urlparse
 
@@ -9,7 +10,7 @@ site_keyword = 'skm'
 site_url = 'http://www.skm.com.tw/'
 # test_url = 'http://www.skm.com.tw/edm/str120/2012_0830_autumn/index.html'
 # test_url = 'http://www.skm.com.tw/edm/?st=str350&dm=20120824_AUTUMN'
-test_url = 'http://www.skm.com.tw/zh-TW/OnLineDM/Detail?CTID=21f47f2f-df94-4045-beef-5e5f5c51cae0&storeId=9e7f2731-ff3c-4d18-b872-ebb2660528f5'
+test_url = 'http://www.skm.com.tw/zh-TW/OnLineDM/Detail?CTID=5b23384c-1bf4-4909-bba3-d1308d3955c6&storeId=4d6c8b25-f31a-4d83-b016-d97973648188'
 
 def parse_query(url):
     o = urlparse.urlparse(url)
@@ -22,6 +23,10 @@ def get_dm_id(url):
 
     return q['CTID'][0]
 
+def get_dm_id_by_html(html):
+    pattern = 'calameo.com\/read\/([0-9a-f]+)" selected'
+    return common.get_first_match(pattern, html)
+
 def get_store_id(url):
     q = parse_query(url)
 
@@ -32,6 +37,10 @@ def get_title_by_value(value, html):
     logging.debug('pattern: %s' % (pattern))
     return common.get_first_match(pattern, html)
 
+def get_dm_title(html):
+    pattern = ' selected >(.*?)</option>'
+    return common.get_first_match(pattern, html)
+
 def get_title(url, html):
     store_id = get_store_id(url)
     dm_id = get_dm_id(url)
@@ -39,7 +48,7 @@ def get_title(url, html):
     logging.debug('id, store: %s, dm: %s' % (store_id, dm_id))
 
     store_title = get_title_by_value(store_id, html).strip()
-    dm_title = get_title_by_value(dm_id, html).strip()
+    dm_title = get_dm_title(html).strip()
 
     title = u'新光三越 %s - %s' % (store_title, dm_title)
 
@@ -53,16 +62,45 @@ def get_charset(html):
     logging.debug('charset = %s' % (charset, ))
     return charset
 
-def find_thumbs(dm_id, html):
-    pattern = '<img src="(/upload/%s/.*?)" />' % (dm_id)
-    thumbs = common.get_all_matched(pattern, html)
+def find_thumbs(obj):
+    if 'content' not in obj:
+        return False
+
+    thumbnail = obj['content']['url']['thumbnail']
+    logging.debug('thumbnail url template:' + thumbnail)
+    pattern = '([0-9]+-[0-9a-z]+)'
+    book_id = common.get_first_match(pattern, thumbnail)
+
+    prefix = 'http://p.calameoassets.com/%s' % (book_id)
+
+    pages = obj['content']['pages']
+    thumbs = ['%s/%s' % (prefix, x['i']['u']) for x in pages]
+
     return thumbs
 
-def get_jpgs(url, html):
-    dm_id = get_dm_id(url)
-    thumbs = find_thumbs(dm_id, html)
+def get_json(dm_id):
+    # http://d.calameo.com/3.0.0/book.php?bkcode=0044215047861f75204a4&callback=_jsonBook
+    url = 'http://d.calameo.com/3.0.0/book.php?bkcode=%s&callback=_jsonBook' % (dm_id)
+    logging.debug('JSON url:' + url)
 
-    jpgs = [urlparse.urljoin(url, thumb.replace('/z_', '/')) for thumb in thumbs]
+    raw = common.get_content_by_url(url)
+    if not raw:
+        return False
+    json_str = raw[len('_jsonBook('):-1]
+    obj = json.loads(json_str)
+    return obj
+
+def get_jpgs(url, html):
+    dm_id = get_dm_id_by_html(html)
+    if not dm_id:
+        logging.warn('Failed to get dm id')
+        return False
+
+    obj = get_json(dm_id)
+    thumbs = find_thumbs(obj)
+
+#     jpgs = [urlparse.urljoin(url, thumb.replace('/z_', '/')) for thumb in thumbs]
+    jpgs = [thumb for thumb in thumbs]
 
     logging.debug('jpgs:' + repr(jpgs))
 
@@ -75,6 +113,10 @@ def downloader(url):
 
     title = get_title(url, html)
     jpgs = get_jpgs(url, html)
+
+    if not jpgs:
+        logging.debug('Failed to get jpgs')
+        return False
 
     common.download_jpgs(title, jpgs)
 
